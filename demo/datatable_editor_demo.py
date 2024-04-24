5790eb5356f655ca56a65510616875753f76352e
Johann <jok.kraft@gmail.com>
2024-04-24 18:14:25 +0200

    Renaming

--- demo/datatable_editor_demo.py
+++ demo/datatable_editor_demo.py
@@ -3,7 +3,7 @@
 from core import DataTableEditor
 
 # Import example data
-from demo_data.data_analysis_test import display_tables_dict
+from demo_data.demo_data import display_tables_dict
 
 # Initiated editor object
 editor = DataTableEditor(datatables=display_tables_dict, port=8052, debug=True)
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name="data_analysis_editor",  # Required
+    name="datatable_editor",  # Required
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
     #
