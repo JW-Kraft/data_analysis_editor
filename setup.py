9244f7e366222ddabb40002d27f01ccb32813440
Johann <jok.kraft@gmail.com>
2024-04-24 18:28:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.0.1",  # Required
+    version="0.0.2",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
@@ -66,7 +66,7 @@ setup(
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url="https://github.com/JW-Kraft/data_analysis_editor",  # Optional
+    url="https://github.com/JW-Kraft/datatable_editor",  # Optional
     # This should be your name or the name of the organization which owns the
     # project.
     author="Johann Kraft",  # Optional
